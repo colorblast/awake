726436604ed16c5349391d55e197ebb30ff0eb25
Jonathan Wong <8844453+colorblast@users.noreply.github.com>
2024-09-05 03:00:17 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Suspend sleep";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        fs = pkgs.lib.fileset;
+
+        nativeBuildInputs = with pkgs; [
+          meson
+          ninja
+          vala
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          pantheon.granite
+          gtk3
+          libgee
+          pantheon.wingpanel
+        ];
+
+        sourceFiles = ./meson.build;
+      in
+      {
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            name = "caffeinated";
+            src = fs.toSource {
+              root = ./.;
+              fileset = sourceFiles;
+            };
+
+            configurePhase = ''
+              echo "copied dir"
+              ls -la
+              meson setup build .
+              '';
+
+            meta = with pkgs.lib; {
+              description = "caffeinated";
+            };
+
+            inherit nativeBuildInputs buildInputs;
+          };
+        };
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; nativeBuildInputs ++ buildInputs;
+          };
+        };
+      });
+}
